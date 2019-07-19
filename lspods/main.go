ada23bfe54cfb786e87afe71e51e3c2bc577dac5
hriships <hrishishinde.p@gmail.com>
2019-07-19 05:39:13 +0530

    adds kubeconfig loader

--- lspods/main.go
+++ lspods/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
@@ -19,6 +21,20 @@ func main() {
 		log.Fatal("unknown resource kind")
 	}
 
-	var cs *kubernetes.Clientset
-	cs.CoreV1().Pods("kube-system").List(meta_v1.ListOptions{})
+	homeDir := os.Getenv("HOME")
+	kubeconfigPath := homeDir + "/.kube/config"
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	pods, err := clientset.CoreV1().Pods("kube-system").List(meta_v1.ListOptions{})
+	if err != nil {
+		log.Fatalf("failed to gte pods %v", err)
+	}
+	fmt.Printf("pods %v ", pods)
 }
