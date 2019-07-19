605c109aabce2aaec4cb647001801c136fd4a68f
hriships <hrishishinde.p@gmail.com>
2019-07-19 05:30:17 +0530

    adds list pods

--- lspods/main.go
+++ lspods/main.go
@@ -1,21 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
+
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 )
 
 func main() {
 	argsWithoutProg := os.Args[1:]
 	if len(argsWithoutProg) == 0 {
 		log.Fatal("no resource is mentioned \nExample command : ls pods")
-
 	}
 
 	if argsWithoutProg[0] != "pods" {
 		log.Fatal("unknown resource kind")
 	}
 
-	fmt.Println("lets print the pods list")
+	var cs *kubernetes.Clientset
+	cs.CoreV1().Pods("kube-system").List(meta_v1.ListOptions{})
 }
